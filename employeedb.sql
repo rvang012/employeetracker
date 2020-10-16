b49d6ecd54b66bbd40aa52ee0ff4cd915480ae35
Ruth Vang <r.vang012@gmail.com>
2020-10-16 16:57:47 -0500

    updating db

---
+++ employeedb.sql
@@ -0,0 +1,30 @@
+drop DATABASE if not EXISTS employees;
+CREATE DATABASE employees;
+use employees;
+create table employee_info
+(
+	id int
+    auto_increment not null. 
+first_name varchar
+	(15) null,
+last_name varchar
+	(15) null,
+role_id int,
+manager_name varchar
+	(15),
+    primary key
+    (id)
+);
+
+create table roles
+(
+	id int
+    auto_increment not null,
+title varchar
+	(15) null,
+salary decimal, 
+department_id int,
+primary key
+	(id)
+);
+
